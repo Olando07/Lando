14a9b625ae2fb35725780daab7d4bc5bed40d1ff
Olando McDonald <olandomcdonald07@gmail.com>
2025-05-02 22:39:54 -0500

    "forgot to addextension to header import"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import "./css/main.css";
 import { Routes, Route } from "react-router";
-import Header from "./components/Header"; 
+import Header from "./components/Header.jsx"; 
 import Footer from "./components/Footer.jsx";
 import Hero from "./components/Hero.jsx";
 import Projects from "./components/Projects.jsx";
