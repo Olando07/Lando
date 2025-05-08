e7f4e8c489a80800972b617f81da7a128aeb4c89
Olando McDonald <olandomcdonald07@gmail.com>
2025-05-08 11:15:57 -0500

    "removing path from vite config"

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,9 +6,4 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: "/Lando/",
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
 });
